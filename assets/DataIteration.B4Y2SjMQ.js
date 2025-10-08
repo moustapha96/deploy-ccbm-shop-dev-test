773d135cab6bc12bd904852398a0cd706801ef25
Al hussein Khouma/Full Stack Developper <68778997+moustapha96@users.noreply.github.com>
2025-10-08 14:38:54 +0000

    Initial commit

---
+++ assets/DataIteration.B4Y2SjMQ.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index.CW09QBtC.js";function c(e){const{datas:t=[],startLength:r,endLength:n,children:s}=e;return a.jsx(a.Fragment,{children:t&&t.length>=n&&t.slice(r,n).map(i=>s({datas:i}))})}export{c as D};
